42113d2d147dde1415ab0c2eec25c94d8c8226c0
“alibayramli” <bayramliali@tutanota.com>
2020-10-07 00:11:54 +0200

    Seperate backend into a folder

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const port = 5000;
-
 const app = express();
 require('dotenv').config();
 app.use(cors());
--- frontend/src/components/TechStack.vue
+++ frontend/src/components/TechStack.vue
@@ -9,6 +9,7 @@ export default {
 	name: 'TechStack',
 	data() {
 		return {
+
 		};
 	},
 };
