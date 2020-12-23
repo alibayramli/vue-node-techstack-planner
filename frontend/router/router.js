6416bbabd93da85b7489ebd14e553f88fe49d810
“alibayramli” <bayramliali@tutanota.com>
2020-12-23 15:10:10 +0100

    Rename hello component to form in router.js

--- frontend/router/router.js
+++ frontend/router/router.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Hello from '../src/components/Form.vue';
+import Form from '../src/components/Form.vue';
 import TechStack from '../src/components/TechStack.vue';
 export const router = createRouter({
 	history: createWebHistory(),
 	routes: [
 		{
-			name: 'hello',
+			name: 'form',
 			path: '/form',
-			component: Hello,
+			component: Form,
 		},
 		{
 			name: 'techstack',
