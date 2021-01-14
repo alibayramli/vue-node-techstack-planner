a7b013288abd42145808b531ad8a4bac9838a235
“alibayramli” <bayramliali@tutanota.com>
2021-01-14 22:14:52 +0100

    Implement saving tool choices

--- frontend/src/components/StatisticsTable.vue
+++ frontend/src/components/StatisticsTable.vue
@@ -42,7 +42,26 @@
 									<td scope="row">{{ index + 1 }}</td>
 									<td scope="row">
 										{{ type === 'developerType' ? convertToStartCase(name) : name }}
-										<ProgLangInfo v-if="isPickable" :name="name" />
+										<ProgLangInfo v-if="hasDetailsCard" :name="name" />
+										<div v-if="isPickable">
+											<a class="tooltip-add" role="button" @click="saveChoice(type,name)">
+												<span class="tooltip-text-add">Click to add the tool</span>
+												<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+													<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
+												</svg>
+
+											</a>
+											<div v-if="choices[type] === name">
+												<p>
+													added
+													<a>
+														<svg style="color:lightgreen" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark-check-fill" viewBox="0 0 16 16">
+															<path fill-rule="evenodd" d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5zm8.854-9.646a.5.5 0 0 0-.708-.708L7.5 7.793 6.354 6.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z" />
+														</svg>
+													</a>
+												</p>
+											</div>
+										</div>
 									</td>
 									<td scope="row" v-if="popularity">{{ popularity }}</td>
 									<td scope="row" v-if="cost">{{ cost }}</td>
@@ -82,6 +101,7 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 import caseConverterMixin from '../mixins/caseConverter';
 import ProgLangInfo from './ProgLangInfo.vue';
 export default {
@@ -105,11 +125,25 @@ export default {
 		isPickable: {
 			type: Boolean,
 		},
+		hasDetailsCard: {
+			type: Boolean,
+		},
 	},
 	data() {
 		return {
 			limit: 5,
 		};
+	},
+	computed: {
+		...mapGetters('userData', {
+			choices: 'getChoices',
+		}),
+	},
+	methods: {
+		saveChoice(type, name) {
+			this.$store.commit('userData/SET_CHOICES', { type, name });
+		},
+
 	},
 };
 </script>
@@ -126,5 +160,29 @@ export default {
 .accordion-button {
   background-color: aliceblue;
   color: black;
+}
+.tooltip-add {
+  position: relative;
+  display: inline-block;
+  border-bottom: 1px dotted black;
+}
+
+.tooltip-add .tooltip-text-add {
+  visibility: hidden;
+  width: 120px;
+  background-color: navy;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px 0;
+
+  /* Position the tooltip */
+  position: absolute;
+  z-index: 2;
+  top: -5px;
+  left: 105%;
+}
+.tooltip-add:hover .tooltip-text-add {
+  visibility: visible;
 }
 </style>
