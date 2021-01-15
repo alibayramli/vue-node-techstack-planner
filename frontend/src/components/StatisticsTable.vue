c8dc5a6377ea9663dc780bf9000e8311d1c20d34
“alibayramli” <bayramliali@tutanota.com>
2021-01-15 19:47:51 +0100

    Check if developer type can be picked based on startup budget

--- frontend/src/components/StatisticsTable.vue
+++ frontend/src/components/StatisticsTable.vue
@@ -44,23 +44,43 @@
 										{{ type === 'developerType' ? convertToStartCase(name) : name }}
 										<ProgLangInfo v-if="hasDetailsCard" :name="name" />
 										<div v-if="isPickableGeneral">
-											<a class="tooltip-add" role="button" @click="selectGeneralChoice(type,name)">
-												<span class="tooltip-text-add">Click to add the tool</span>
-												<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
-													<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
-												</svg>
+											<div v-if="type === 'developerType'">
+												<a :disabled="true" class="tooltip-add" role="button" v-if="startupBudget > parseInt(cost)" @click="selectGeneralChoice(type,name)">
+													<span class="tooltip-text-add">Click to add the tool </span>
+													<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+														<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
+													</svg>
 
-											</a>
-											<div v-if="generalChoices[type] === name">
-												<p>
-													added
-													{{ generalChoices }}
-													<a>
-														<svg style="color:lightgreen" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark-check-fill" viewBox="0 0 16 16">
-															<path fill-rule="evenodd" d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5zm8.854-9.646a.5.5 0 0 0-.708-.708L7.5 7.793 6.354 6.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z" />
-														</svg>
-													</a>
-												</p>
+												</a>
+												<div v-if="generalChoices[type] === name">
+													<p>
+														added
+														<a>
+															<svg style="color:lightgreen" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark-check-fill" viewBox="0 0 16 16">
+																<path fill-rule="evenodd" d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5zm8.854-9.646a.5.5 0 0 0-.708-.708L7.5 7.793 6.354 6.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z" />
+															</svg>
+														</a>
+													</p>
+												</div>
+											</div>
+											<div v-else>
+												<a class="tooltip-add" role="button" @click="selectGeneralChoice(type,name)">
+													<span class="tooltip-text-add">Click to add the tool </span>
+													<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+														<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
+													</svg>
+
+												</a>
+												<div v-if="generalChoices[type] === name">
+													<p>
+														added
+														<a>
+															<svg style="color:lightgreen" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark-check-fill" viewBox="0 0 16 16">
+																<path fill-rule="evenodd" d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5zm8.854-9.646a.5.5 0 0 0-.708-.708L7.5 7.793 6.354 6.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z" />
+															</svg>
+														</a>
+													</p>
+												</div>
 											</div>
 										</div>
 										<div v-if="isPickableTeam">
@@ -121,7 +141,7 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapState, mapGetters } from 'vuex';
 import caseConverterMixin from '../mixins/caseConverter';
 import ProgLangInfo from './ProgLangInfo.vue';
 export default {
@@ -166,6 +186,9 @@ export default {
 			generalChoices: 'getGeneralChoices',
 			teamChoices: 'getTeamChoices',
 		}),
+		...mapState('startupData', {
+			startupBudget: 'budget',
+		}),
 	},
 	methods: {
 		selectGeneralChoice(type, name) {
@@ -174,7 +197,6 @@ export default {
 		selectTeamChoice(header, type, name) {
 			this.$store.commit('userData/SET_TEAM_CHOICES', { header, type, name });
 		},
-
 	},
 };
 </script>
