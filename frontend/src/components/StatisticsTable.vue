8868d0b56d3b85dfd10a145b5e574fb303ad6927
“alibayramli” <bayramliali@tutanota.com>
2021-01-25 18:15:29 +0100

    Remove unnecessary disabled attr

--- frontend/src/components/StatisticsTable.vue
+++ frontend/src/components/StatisticsTable.vue
@@ -45,7 +45,7 @@
 										<ProgLangInfo v-if="hasDetailsCard" :name="name" />
 										<div v-if="isPickableGeneral">
 											<div v-if="type === 'developerType'">
-												<a :disabled="true" class="tooltip-add" role="button" v-if="startupBudget > parseInt(cost)" @click="selectGeneralChoice(type,name)">
+												<a class="tooltip-add" role="button" v-if="startupBudget > parseInt(cost)" @click="selectGeneralChoice(type,name)">
 													<span class="tooltip-text-add">Click to add the tool </span>
 													<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
 														<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
