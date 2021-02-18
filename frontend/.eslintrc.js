38654713e7cc4ffc7cd34ba882111e4dee084080
Ali Bayramli <bayramliali@tutanota.com>
2021-02-18 20:55:34 +0100

    Update eslint-vue configurations

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -7,10 +7,6 @@ module.exports = {
 	extends: [
 		'eslint:recommended',
 		'plugin:vue/vue3-recommended',
-		'plugin:vue/base',
-		'plugin:vue/vue3-essential',
-		'plugin:vue/vue3-strongly-recommended',
-		'plugin:vue/vue3-recommended',
 	],
 	rules: {
 		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
