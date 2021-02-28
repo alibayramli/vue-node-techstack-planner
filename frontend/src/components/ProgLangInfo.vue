ce271a4fd2c9749338cd0d23fc1bea57edbb0427
Ali Bayramli <bayramliali@tutanota.com>
2021-02-28 14:36:05 +0100

    Style statistics table tooltips

--- frontend/src/components/ProgLangInfo.vue
+++ frontend/src/components/ProgLangInfo.vue
@@ -39,10 +39,11 @@ export default {
 .tooltip-view  .tooltip-text-view{
   visibility: hidden;
   width: 120px;
-  background-color: navy;
+  background-color:#0d6efd;
   color: #fff;
   text-align: center;
   border-radius: 6px;
+  margin: 0 5px;
   padding: 5px 0;
 
   /* Position the tooltip */
