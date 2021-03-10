4bca6d3f5b392fe0fe17bb1c5d29df5e4cc3e0d3
Ali Bayramli <bayramliali@tutanota.com>
2021-03-10 14:51:09 +0100

    seperate icon elements in team choice picks component

--- frontend/src/components/TeamChoicePicks.vue
+++ frontend/src/components/TeamChoicePicks.vue
@@ -1,29 +1,23 @@
 <template>
 	<div>
-		<a class="tooltip-add" role="button" @click="selectTeamChoice(header,type,name)">
-			<span class="tooltip-text-add">Click to add the tool</span>
-			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
-				<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
-			</svg>
-
-		</a>
-		<div v-if="teamChoices.some((choice) => choice[header] ? choice[header][type] === name : null)">
-			<p>
-				added
-				<a>
-					<svg style="color:lightgreen" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark-check-fill" viewBox="0 0 16 16">
-						<path fill-rule="evenodd" d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5zm8.854-9.646a.5.5 0 0 0-.708-.708L7.5 7.793 6.354 6.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z" />
-					</svg>
-				</a>
-			</p>
+		<button class="tooltip-add" @click="selectTeamChoice(header,type,name)">
+			<AddChoicesIcon type="addChoice" />
+		</button>
+		<div v-if="isTeamChoiceAdded">
+			<AddChoicesIcon type="choiceAdded" />
 		</div>
 	</div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+import AddChoicesIcon from './AddChoicesIcon.vue';
+
 export default {
 	name: 'TeamChoicePicks',
+	components: {
+		AddChoicesIcon,
+	},
 	props: {
 		header: {
 			type: String,
@@ -45,6 +39,10 @@ export default {
 		...mapGetters('userData', {
 			teamChoices: 'getTeamChoices',
 		}),
+		isTeamChoiceAdded() {
+			return this.teamChoices
+				.some(choice => choice[this.header] && choice[this.header][this.type] === this.name);
+		},
 	},
 	methods: {
 		selectTeamChoice(header, type, name) {
@@ -55,6 +53,11 @@ export default {
 </script>
 
 <style scoped>
+button {
+  outline: none;
+  background: transparent;
+  border: 1px solid transparent;
+}
 .tooltip-add {
   position: relative;
   display: inline-block;
