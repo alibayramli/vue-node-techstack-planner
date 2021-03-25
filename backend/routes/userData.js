c0edc4701f4e9eeaa728bdca64b746aaed44710e
Ali Bayramli <bayramliali@tutanota.com>
2021-03-25 16:20:18 +0100

    Send startup infos by user id

--- backend/routes/userData.js
+++ backend/routes/userData.js
@@ -1,21 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const INTERNAL_SERVER_ERROR = 500;
-const UserChoice = require('../models/userChoice.js');
+const { verifyAccessToken } = require('../helpers/jwt');
+const Detail = require('../models/detail.js');
 
-router.get('/startup-choices', (req, res) => {
+router.get('/startup-choices', verifyAccessToken, (req, res) => {
 	try {
-		res.send('saved choices from backend');
+		Detail
+			.find({})
+			.populate('author', { choices: 1 })
+			.exec((err, results) => {
+				const userChoices = results.map((result) => {
+					return {
+						startupId: result._id,
+						startupName: result.startupName,
+						creationDate: result.creationDate,
+						choices: result.choices,
+					};
+				});
+				res.send(userChoices);
+			});
 	} catch (err) {
 		console.log(err);
 		res.status(INTERNAL_SERVER_ERROR).send(err);
 	}
 });
 
-router.post('/', async (req, res) => {
+router.post('/', verifyAccessToken, async (req, res) => {
 	try {
-		const newStartupChoice = new UserChoice(req.body);
-		console.log(req.body);
+		req.body.author = req.user.aud;
+		const newStartupChoice = new Detail(req.body);
 		await newStartupChoice.save();
 		res.send(newStartupChoice);
 	} catch (err) {
