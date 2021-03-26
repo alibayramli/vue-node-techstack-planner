c3c8c7a9742f6f915c56a91545b690fed943e9c6
Ali Bayramli <bayramliali@tutanota.com>
2021-03-26 16:45:55 +0100

    Add text if there is no saved startup

--- frontend/src/components/UserChoices.vue
+++ frontend/src/components/UserChoices.vue
@@ -19,24 +19,29 @@
 		</div>
 		<hr>
 		<h1>Saved Startups</h1>
-		<div class="row" v-for="startup of savedStartups" :key="startup">
-			<div>
-				<div class="card">
-					<div class="card-body">
-						<h5 class="card-title">Name of the startup: <b>{{ startup.startupName }}</b></h5>
-						<p class="card-text">Creation date:  <b>{{ showCreationDate(startup.creationDate) }}</b></p>
-						<button type="button"
-							class="btn btn-primary"
-							@click="viewStartup(startup.startupId)"
-						>
-							View
-						</button>
+		<div v-if="savedStartups.length">
+			<div class="row" v-for="startup of savedStartups" :key="startup">
+				<div>
+					<div class="card">
+						<div class="card-body">
+							<h5 class="card-title">Name of the startup: <b>{{ startup.startupName }}</b></h5>
+							<p class="card-text">Creation date:  <b>{{ showCreationDate(startup.creationDate) }}</b></p>
+							<button type="button"
+								class="btn btn-primary"
+								@click="viewStartup(startup.startupId)"
+							>
+								View
+							</button>
+						</div>
 					</div>
+					<br>
 				</div>
-				<br>
 			</div>
 		</div>
-
+		<div v-else>
+			No saved data,  <router-link to="/form">start adding</router-link>
+		</div>
+		<hr>
 		<router-view />
 	</div>
 </template>
