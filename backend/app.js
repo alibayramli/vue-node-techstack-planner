27d9c9148e6a24921b1ff9338b8a0652faab44b3
Ali Bayramli <bayramliali@tutanota.com>
2021-03-31 10:30:56 +0200

    Refactor server file

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-require('./helpers/init_mongodb');
-require('./helpers/init_redis');
-const statisticsDataRoutes = require('./routes/statisticsData');
-const startupDataRoutes = require('./routes/startupData');
-const formDataRoutes = require('./routes/formData');
-const programmingLanguagesDataRoutes = require('./routes/programmingLanguagesData');
-const userDataRoutes = require('./routes/userData');
-const authDataRoutes = require('./routes/authData');
-const port = 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/statistics-data', statisticsDataRoutes);
+const port = 5000;
+require('./helpers/init_mongodb');
+require('./helpers/init_redis');
+const authDataRoutes = require('./routes/authData');
+const startupDataRoutes = require('./routes/startupData');
+const techStackDataRoutes = require('./routes/techStackData');
+app.use('/techstack-data', techStackDataRoutes);
 app.use('/startup-data', startupDataRoutes);
-app.use('/form-data', formDataRoutes);
-app.use('/programming-languages-data', programmingLanguagesDataRoutes);
-app.use('/user-data', userDataRoutes);
 app.use('/auth-data', authDataRoutes);
 
 app.listen(port, (err) => {
