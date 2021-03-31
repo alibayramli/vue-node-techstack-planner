de0767e8c8a2778ebb57b62806f0301e6b276d6d
Ali Bayramli <bayramliali@tutanota.com>
2021-03-31 12:05:55 +0200

    Update vuex module name

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -1,14 +1,14 @@
 import { createStore } from 'vuex';
 import statisticsData from './modules/statisticsData';
 import startupFormData from './modules/startupFormData';
-import programmingLanguagesData from './modules/programmingLanguagesData';
+import progLangInfoData from './modules/progLangInfoData';
 import startupChoicesData from './modules/startupChoicesData';
 import authData from './modules/authData';
 export const store = createStore({
 	modules: {
 		statisticsData,
 		startupFormData,
-		programmingLanguagesData,
+		progLangInfoData,
 		startupChoicesData,
 		authData,
 	},
