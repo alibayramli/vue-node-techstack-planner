bcec5d4af280e5b5f835751fc4e60a70622cccb5
Ali Bayramli <bayramliali@tutanota.com>
2021-03-31 10:35:17 +0200

    Create route for startups

--- backend/routes/startupData.js
+++ backend/routes/startupData.js
@@ -1,15 +1,61 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
-const { verifyAccessToken } = require('../helpers/jwt');
-const statisticsFile = './data/statistics.json';
 const INTERNAL_SERVER_ERROR = 500;
-router.post('/', verifyAccessToken, (req, res) => {
+const { verifyAccessToken } = require('../helpers/jwt');
+const Startup = require('../models/startup.js');
+
+router.get('/user-startups', verifyAccessToken, async (req, res) => {
 	try {
-		const statistics = JSON.parse(fs.readFileSync(statisticsFile, 'utf8'));
-		const { size, location, field } = req.body;
-		const tools = statistics.general.softwareType[field].popularLanguages;
-		res.send(tools);
+		const details = await Startup.find({ author: req.user.aud });
+		const startupDetails = details.map((detail) => {
+			return {
+				startupId: detail._id,
+				startupName: detail.startupName,
+				startupSize: detail.startupSize,
+				startupLocation: detail.startupLocation,
+				startupField: detail.startupField,
+				startupBudget: detail.startupBudget,
+				creationDate: detail.creationDate,
+				choices: detail.choices,
+			};
+		});
+		res.send(startupDetails);
+	} catch (err) {
+		console.log(err);
+		res.status(INTERNAL_SERVER_ERROR).send(err);
+	}
+});
+
+router.post('/new-startup', verifyAccessToken, async (req, res) => {
+	try {
+		req.body.author = req.user.aud;
+		const newStartupInfo = new Startup(req.body);
+		await newStartupInfo.save();
+		res.send(newStartupInfo);
+	} catch (err) {
+		console.log(err);
+		res.status(INTERNAL_SERVER_ERROR).send(err);
+	}
+});
+
+router.patch('/edit-startup/:startupId', verifyAccessToken, async (req, res) => {
+	try {
+		req.body.author = req.user.aud;
+		const startupToUpdate = await Startup
+			.findByIdAndUpdate(req.params.startupId, req.body, { new: true });
+		res.send(startupToUpdate);
+	} catch (err) {
+		console.log(err);
+		res.status(INTERNAL_SERVER_ERROR).send(err);
+	}
+});
+
+router.delete('/delete-startup/:startupId', verifyAccessToken, async (req, res) => {
+	try {
+		req.body.author = req.user.aud;
+		const startupToDelete = await Startup
+			.findByIdAndDelete(req.params.startupId);
+		res.send(startupToDelete);
 	} catch (err) {
 		console.log(err);
 		res.status(INTERNAL_SERVER_ERROR).send(err);
