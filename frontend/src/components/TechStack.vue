f8ababf134c6b639352a86af531692249c9a9f41
Ali Bayramli <bayramliali@tutanota.com>
2021-03-31 10:34:06 +0200

    Remove toolsdata module from vuex

--- frontend/src/components/TechStack.vue
+++ frontend/src/components/TechStack.vue
@@ -31,7 +31,7 @@ export default {
 		return {};
 	},
 	computed: {
-		...mapState('toolsData', {
+		...mapState('startupFormData', {
 			tools: 'tools',
 		}),
 		...mapGetters('statisticsData', {
--- frontend/store/index.js
+++ frontend/store/index.js
@@ -1,7 +1,6 @@
 import { createStore } from 'vuex';
 import statisticsData from './modules/statisticsData';
 import startupFormData from './modules/startupFormData';
-import toolsData from './modules/toolsData';
 import programmingLanguagesData from './modules/programmingLanguagesData';
 import startupChoicesData from './modules/startupChoicesData';
 import authData from './modules/authData';
@@ -9,7 +8,6 @@ export const store = createStore({
 	modules: {
 		statisticsData,
 		startupFormData,
-		toolsData,
 		programmingLanguagesData,
 		startupChoicesData,
 		authData,
