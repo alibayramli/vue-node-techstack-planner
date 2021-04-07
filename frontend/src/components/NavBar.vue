b8a99d78750f79194ef6b519df8b74411e5e20d8
Ali Bayramli <bayramliali@tutanota.com>
2021-04-07 22:23:24 +0200

    Move navigation info into navbar component

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,88 @@
+<template>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#">Tech Stack Planner</a>
+			<button
+				class="navbar-toggler"
+				type="button"
+				data-bs-toggle="collapse"
+				data-bs-target="#navbarSupportedContent"
+				aria-controls="navbarSupportedContent"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<span class="navbar-toggler-icon" />
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+					<li class="nav-item">
+						<router-link class="nav-link" to="/home"> Home </router-link>
+					</li>
+					<li class="nav-item" v-if="isLoggedIn">
+						<router-link class="nav-link" to="/form"> Form </router-link>
+					</li>
+					<li class="nav-item" v-if="isLoggedIn">
+						<router-link class="nav-link" to="/techstack">
+							Tech Stack
+						</router-link>
+					</li>
+					<li class="nav-item" v-if="isLoggedIn">
+						<router-link class="nav-link" to="/user-startups">
+							Your Startups
+						</router-link>
+					</li>
+				</ul>
+				<ul class="d-flex navbar-nav mb-2 mb-lg-0">
+					<li class="nav-item" v-if="!isLoggedIn">
+						<router-link class="nav-link" to="/">
+							Login
+						</router-link>
+					</li>
+					<li class="nav-item" v-if="!isLoggedIn">
+						<router-link class="nav-link" to="/signup">
+							Signup
+						</router-link>
+					</li>
+					<li class="nav-item" v-if="isLoggedIn">
+						<a class="nav-link" style="cursor:pointer">
+							{{ fullName }}
+						</a>
+					</li>
+					<li class="nav-item" v-if="isLoggedIn">
+						<a class="nav-link" style="cursor:pointer" @click="logout()">
+							Logout
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+export default {
+	name: 'NavBar',
+	data() {
+		return {};
+	},
+	computed: {
+		...mapGetters('authData', {
+			isLoggedIn: 'isLoggedIn',
+			fullName: 'getFullName',
+		}),
+	},
+	methods: {
+		...mapActions('authData', {
+			sendLogOutInfo: 'sendLogOutInfo',
+		}),
+		async logout() {
+			await this.sendLogOutInfo();
+		},
+	},
+};
+</script>
+
+<style scoped>
+
+</style>
