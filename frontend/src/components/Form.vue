78c7aff6c5fbfcd97c2d579542b6b909f64bd050
Ali Bayramli <bayramliali@tutanota.com>
2021-04-09 13:08:44 +0200

    Refactor form component design

--- frontend/src/components/Form.vue
+++ frontend/src/components/Form.vue
@@ -1,105 +1,98 @@
 <template>
 	<div id="app">
-		<form
-			class="startup-form"
-			@submit.prevent="submit"
-		>
-			<fieldset>
-				<legend>Tech Stack Specification Form</legend>
+		<div class="startup-form">
+			<legend>Tech Stack Specification Form</legend>
+			<div class="mb-3">
+				<input type="text" class="form-control form-label" placeholder="Name of the startup" aria-label="Startup Name" required v-model="startupName">
+			</div>
 
-				<hr>
-				<div class="mb-3">
-					<input type="text" class="form-control form-label" placeholder="Name of the startup" aria-label="Startup Name" required v-model="startupName">
-				</div>
+			<div class="mb-3">
+				<label for="size" class="form-label">Startup size</label>
+				<select
+					class="form-select"
 
-				<div class="mb-3">
-					<label for="size" class="form-label">Startup size</label>
-					<select
-						class="form-select"
+					aria-label="select"
+					v-model="startupSize"
+				>
+					<option v-for="size in sizes" :key="size" :value="size">
+						{{ $convertToStartCase(size) }}
+					</option>
+				</select>
+				<div class="invalid-feedback">Please select</div>
+			</div>
 
-						aria-label="select"
-						v-model="startupSize"
+			<div class="mb-3">
+				<label for="location" class="form-label">Startup Location</label>
+				<select
+					class="form-select"
+					aria-label="select"
+					v-model="startupLocation"
+				>
+					<option
+						v-for="location in locations"
+						:value="location"
+						:key="location"
 					>
-						<option v-for="size in sizes" :key="size" :value="size">
-							{{ $convertToStartCase(size) }}
-						</option>
-					</select>
-					<div class="invalid-feedback">Please select</div>
-				</div>
+						{{ location }}
+					</option>
+				</select>
+				<div class="invalid-feedback">Please select</div>
+			</div>
 
-				<div class="mb-3">
-					<label for="location" class="form-label">Startup Location</label>
-					<select
-						class="form-select"
-						aria-label="select"
-						v-model="startupLocation"
-					>
-						<option
-							v-for="location in locations"
-							:value="location"
-							:key="location"
-						>
-							{{ location }}
-						</option>
-					</select>
-					<div class="invalid-feedback">Please select</div>
-				</div>
+			<div class="mb-3">
+				<label for="field" class="form-label">Startup Field</label>
+				<select
+					class="form-select"
+					required
+					aria-label="select"
+					v-model="startupField"
+				>
+					<option v-for="field in fields" :key="field" :value="field">
+						{{ $convertToStartCase(field) }}
+					</option>
+				</select>
+				<div class="invalid-feedback">Please select</div>
+			</div>
+			<div class="mb-3">
+				<label for="field" class="form-label">Does Deployment Speed Matter? </label>
+				<select
+					class="form-select"
+					required
+					aria-label="select"
+					v-model="startupDeploymentSpeed"
+				>
+					<option v-for="speed in fastDeploymentInfos" :key="speed" :value="speed">
+						{{ $convertToStartCase(speed) }}
+					</option>
+				</select>
+				<div class="invalid-feedback">Please select</div>
+			</div>
+			<div class="mb-3">
+				<label for="budget" class="form-label">Startup Budget</label>
+				<input type="text" class="form-control form-label"
+					placeholder="Startup Budget" aria-label="Budget"
+					required v-model="startupBudget"
+					@keypress=" $isValidStartupBudget($event,startupBudget)"
+				>
+				<div class="form-text">Please include average annual salary per person. (e.g 65 -> 65000 USD)</div>
+			</div>
 
-				<div class="mb-3">
-					<label for="field" class="form-label">Startup Field</label>
-					<select
-						class="form-select"
-						required
-						aria-label="select"
-						v-model="startupField"
-					>
-						<option v-for="field in fields" :key="field" :value="field">
-							{{ $convertToStartCase(field) }}
-						</option>
-					</select>
-					<div class="invalid-feedback">Please select</div>
-				</div>
-				<div class="mb-3">
-					<label for="field" class="form-label">Does Deployment Speed Matter? </label>
-					<select
-						class="form-select"
-						required
-						aria-label="select"
-						v-model="startupDeploymentSpeed"
-					>
-						<option v-for="speed in fastDeploymentInfos" :key="speed" :value="speed">
-							{{ $convertToStartCase(speed) }}
-						</option>
-					</select>
-					<div class="invalid-feedback">Please select</div>
-				</div>
-				<div class="mb-3">
-					<label for="budget" class="form-label">Startup Budget</label>
-					<input type="text" class="form-control form-label"
-						placeholder="Startup Budget" aria-label="Budget"
-						required v-model="startupBudget"
-						@keypress=" $isValidStartupBudget($event,startupBudget)"
-					>
-					<div class="form-text">Please include average annual salary per person. (e.g 65 -> 65000 USD)</div>
-				</div>
-
-				<div class="mb-3">
-					<button
-						class="btn btn-primary"
-						type="submit"
-						:disabled="(!startupName || !startupField || !startupDeploymentSpeed || !startupBudget )
-							|| isSubmitFormClicked"
-					>
-						Submit form
-						<div
-							class="spinner-border spinner-border-sm"
-							role="status"
-							v-if="isSubmitFormSpinnerActive"
-						/>
-					</button>
-				</div>
-			</fieldset>
-		</form>
+			<div class="mb-3">
+				<button
+					class="btn btn-primary"
+					:disabled="(!startupName || !startupField || !startupDeploymentSpeed || !startupBudget )
+						|| isSubmitFormClicked"
+					@click="submitForm"
+				>
+					Submit form
+					<div
+						class="spinner-border spinner-border-sm"
+						role="status"
+						v-if="isSubmitFormSpinnerActive"
+					/>
+				</button>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -175,7 +168,7 @@ export default {
 		...mapActions('startupFormData', {
 			createStartupQuery: 'createStartupQuery',
 		}),
-		async submit() {
+		async submitForm() {
 			this.$store.commit('startupChoicesData/RESET_STARTUP_CHOICES');
 			this.isSubmitFormSpinnerActive = true;
 			this.isSubmitFormClicked = true;
@@ -190,13 +183,12 @@ export default {
 
 <style scoped>
 .startup-form {
-  margin: auto;
-  margin-top: 5rem;
-  padding: 15px 30px;
-  width: 40rem;
+  margin: 5rem auto;
+  padding:2rem;
+  max-width: 50rem;
   background-color: #fff;
   -webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12),
     0 1px 2px rgba(0, 0, 0, 0.24);
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(41, 0, 60, 0.24);
 }
 </style>
