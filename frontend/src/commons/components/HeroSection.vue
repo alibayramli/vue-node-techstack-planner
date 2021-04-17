9e073572dac8c169ac85000cb4b9577b4e87adb1
Ali Bayramli <bayramliali@tutanota.com>
2021-04-17 17:39:24 +0200

    Add hero component

---
+++ frontend/src/commons/components/HeroSection.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="bg-dark bg-gradient text-secondary px-4 py-5 text-center">
+		<div class="py-5">
+			<h1 class="display-5 fw-bold text-white">Tech service for your startup</h1>
+			<div class="col-lg-6 mx-auto">
+				<p class="fs-5 mb-4">
+					Tech stack planner lets startup owners find tech stacks that are
+					necessary for their their products including programming languages, frameworks
+					as well as collaboration tools
+				</p>
+				<div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+					<router-link
+						class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold"
+						to="/signup"
+					>
+						Get started
+					</router-link>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
